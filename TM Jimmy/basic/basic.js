23e0a6562761689f5299bbe75fc1090f896d7505
Jimmy CHAVES <jimmybakerchaves@gmail.com>
2022-09-26 10:41:17 +0200

    exercise 1 done

---
+++ TM Jimmy/basic/basic.js
@@ -0,0 +1,15 @@
+// exercise 1
+let array = [25, 58, 2, 96, 46, 22, 178, 105];
+let max = 0 ;
+
+for (let i=0 ; i < array.length; i++) {
+    if (max < array[i]) {
+        max = array[i];
+    }
+   
+}
+ console.log(max);
+
+ //exercise 2
+
+ 
